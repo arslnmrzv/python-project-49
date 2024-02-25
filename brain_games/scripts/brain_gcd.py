d01cba681a77c06f4740c260c8156773ab483ba1
Arslan Morozov <mav1324@outlook.com>
2024-02-25 20:43:05 +0300

    correction brain_gcd.py

--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -21,12 +21,12 @@ def main():
         random_num2 = randint(0, 100)
         print(f"Question: {random_num1} {random_num2}")
         answer = prompt.string("Your answer: ")
-
-        if answer == gcd(random_num1, random_num2):
+        correct_answer = gcd(random_num1, random_num2)
+        if answer == str(correct_answer):
             print("Correct!")
             n += 1
         else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {gcd(random_num1, random_num2)}.")
+            print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}.")
             print(f"Let's try again,{name}!")
             return
     print(f"Congratuletions,{name}!")
