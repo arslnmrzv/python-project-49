54a0af657fe3edc871600c47493fcc8f1b4f08ed
Arslan Morozov <mav1324@outlook.com>
2024-02-28 22:42:06 +0300

    make calc game architecture

---
+++ brain_games/games/game_calc.py
@@ -0,0 +1,22 @@
+import random
+
+
+DESCRIPTION = 'What is the result of the expression?'
+
+
+def game():
+    random_num1 = random.randint(1, 10)
+    random_num2 = random.randint(1, 10)
+    sign = ["+", "-", "*"]
+    random_sign = random.choice(sign)
+    question = (f"{random_num1} {random_sign} {random_num2}")
+    if random_sign == "+":
+        int_answer = random_num1 + random_num2
+        correct_answer = str(int_answer)
+    elif random_sign == "-":
+        int_answer = random_num1 - random_num2
+        correct_answer = str(int_answer)
+    elif random_sign == "*":
+        int_answer = random_num1 * random_num2
+        correct_answer = str(int_answer)
+    return question, correct_answer
--- brain_games/scripts/brain_calc.py
+++ brain_games/scripts/brain_calc.py
@@ -1,38 +1,10 @@
 #!/usr/bin/env python3
-import random
-
-import prompt
+from brain_games.games import game_calc
+from brain_games.engine import engine
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f"Hello, {name}!")
-    print('What is the result of the expression?')
-    n = 0
-    while n < 3:
-        random_num1 = random.randint(1, 10)
-        random_num2 = random.randint(1, 10)
-        sign = ["+", "-", "*"]
-        random_sign = random.choice(sign)
-        if random_sign == "+":
-            correct_answer = random_num1 + random_num2
-        elif random_sign == "-":
-            correct_answer = random_num1 - random_num2
-        elif random_sign == "*":
-            correct_answer = random_num1 * random_num2
-        print(f"Question: {random_num1} {random_sign} {random_num2}")
-        answer = prompt.string("Your answer: ")
-        if answer == str(correct_answer):
-            print("Correct!")
-            n += 1
-        else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}.")
-            print(f"Let's try again,{name}!")
-            return
-
-    print(f"Congratuletions,{name}!")
-
+    engine(game_calc)
 
 if __name__ == '__main__':
     main()
