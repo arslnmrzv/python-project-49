0640b37fb32a2ff3fc2e855d133dcd5a4ec4ebf3
Arslan Morozov <mav1324@outlook.com>
2024-02-28 23:53:15 +0300

    make progression game

---
+++ brain_games/scripts/brain_progression.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python3
+from brain_games.games import game_progression
+from brain_games.engine import engine
+
+
+def main():
+    engine(game_progression)
+
+if __name__ == '__main__':
+    main()
+
+
