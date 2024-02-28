d5ba3be5aff1cc94aec213fda8a2f4a232fb39cb
Arslan Morozov <mav1324@outlook.com>
2024-02-28 23:17:49 +0300

    make gcd game architecture

---
+++ brain_games/games/game_gcd.py
@@ -0,0 +1,20 @@
+import random
+
+
+DESCRIPTION = 'What is the result of the expression?'
+
+
+def gcd(num1, num2):
+    if num2 == 0:
+        return num1
+    else:
+        return gcd(num2, num1 % num2)
+    
+    
+def game():
+    random_num1 = random.randint(0, 100)
+    random_num2 = random.randint(0, 100)
+    question = (f"{random_num1} {random_num2}")
+    right_answer = gcd(random_num1, random_num2)
+    correct_answer = str(right_answer)
+    return question, correct_answer
--- brain_games/scripts/brain_gcd.py
+++ brain_games/scripts/brain_gcd.py
@@ -1,36 +1,9 @@
-#!/usr/bin/env python3
-from random import randint
-import prompt
-
-
-def gcd(num1, num2):
-    if num2 == 0:
-        return num1
-    else:
-        return gcd(num2, num1 % num2)
+from brain_games.games import game_gcd
+from brain_games.engine import engine
 
 
 def main():
-    print("Welcome to the Brain Games!")
-    name = prompt.string('May I have your name? ')
-    print(f"Hello, {name}!")
-    print('Find the greatest common divisor of given numbers.')
-    n = 0
-    while n < 3:
-        random_num1 = randint(0, 100)
-        random_num2 = randint(0, 100)
-        print(f"Question: {random_num1} {random_num2}")
-        answer = prompt.string("Your answer: ")
-        correct_answer = gcd(random_num1, random_num2)
-        if answer == str(correct_answer):
-            print("Correct!")
-            n += 1
-        else:
-            print(f"{answer} is wrong answer ;(. Correct answer was {correct_answer}.")
-            print(f"Let's try again,{name}!")
-            return
-    print(f"Congratuletions,{name}!")
-
+    engine(game_gcd)
 
 if __name__ == '__main__':
     main()
