a4e8f611eff3aabcc9b51a93fec16e76471ef117
Arslan Morozov <mav1324@outlook.com>
2024-02-29 00:29:23 +0300

    make prime game

---
+++ brain_games/scripts/brain_prime.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+
+
+from brain_games.games import game_prime
+from brain_games.engine import engine
+
+
+def main():
+    engine(game_prime)
+
+
+if __name__ == '__main__':
+    main()
