73c240f81aad7b9aadc806be0349c10a0b28e5e2
Arslan Morozov <mav1324@outlook.com>
2024-03-06 22:25:37 +0300

    correction prime game

--- brain_games/games/game_prime.py
+++ brain_games/games/game_prime.py
@@ -8,10 +8,14 @@ DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 def is_prime(n):
     if n <= 1:
         return False
-    for i in range(2, int(n**0.5) + 1):
+    if n == 2:
+        return True
+    if n % 2 == 0:
+        return False
+    for i in range(3, int(n**0.5)+1, 2):
         if n % i == 0:
             return False
-        return True 
+        return True
 
 
 def game():
